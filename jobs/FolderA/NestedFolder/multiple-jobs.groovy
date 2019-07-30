eca6ed82dcb76416685e5badd603376998a8ba7e
Oliver Gondža <ogondza@gmail.com>
2019-07-30 15:42:19 +0200

    Some more cleanup

--- jobs/FolderA/NestedFolder/multiple-jobs.groovy
+++ jobs/FolderA/NestedFolder/multiple-jobs.groovy
@@ -6,19 +6,7 @@ for (String plugin : plugins) {
                 script("""
                     node() {
                         git "https://github.com/jenkinsci/${plugin}-plugin"
-                        sh '/qa/tools/opt/maven-3.6.0/bin/mvn -B clean cobertura:cobertura -Dcobertura.report.format=xml -Dmaven.test.failure.ignore=true'
-                        junit '**/target/surefire-reports/**/*.xml'
-                        cobertura([
-                            autoUpdateHealth: false,
-                            autoUpdateStability: false,
-                            coberturaReportFile: '**/target/site/cobertura/coverage.xml',
-                            failUnhealthy: false,
-                            failUnstable: false,
-                            maxNumberOfBuilds: 0,
-                            onlyStable: false,
-                            sourceEncoding: 'ASCII',
-                            zoomCoverageChart: false
-                        ])
+                        sh 'cat pom.xml'
                     }
                 """)
                 sandbox()
